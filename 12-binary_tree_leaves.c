558fdb909c725f42f38c0381efb486f42229c220
DaisyAram <daisyarama03@gmail.com>
2024-02-27 02:52:28 -0800

    leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,17 +6,15 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaf = 0, l = 0, r = 0;
-
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	else
+
+	if (tree->left == NULL && tree->right == NULL)
 	{
-		l = binary_tree_leaves(tree->left);
-		r = binary_tree_leaves(tree->right);
-		leaf = l + r;
-		return ((!l && !r) ? leaf + 1 : leaf + 0);
+		return (1);
 	}
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
