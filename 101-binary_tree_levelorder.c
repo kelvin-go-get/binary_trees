15a9339ad29b4b5f764ff97caa7723e60523bb5c
DaisyAram <daisyarama03@gmail.com>
2024-02-28 01:41:39 -0800

    bertty

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -21,7 +21,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
 		return ((left_height > right_height) ? left_height : right_height);
-	
+
 	}
 }
 /**
